0788565114c01ee78e51171912f431b57d9a19a2
Coolgolf1 <coolgolfmaster@gmail.com>
2025-11-19 18:35:42 +0100

    change docs

--- Arquitectura2526/Montecarlo/montecarlo_spark.py
+++ Arquitectura2526/Montecarlo/montecarlo_spark.py
@@ -12,10 +12,10 @@ Description:  This script performs a Monte Carlo simulation using the
               distribution.
 
 Usage:
-    python montecarlo_dask.py <num_samples>
+    python montecarlo_spark.py <num_samples>
 
     Example:
-        python montecarlo_dask.py 1000000
+        python montecarlo_spark.py 1000000
 
 Arguments:
     num_samples : int
@@ -23,7 +23,7 @@ Arguments:
 
 Dependencies:
     - Python >= 3.8
-    - spark
+    - pyspark.sql
     - numpy
 
 Functions:
